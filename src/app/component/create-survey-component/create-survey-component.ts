5d2ce4511c77d68cd991f07f90f2b77aadf21b1c
Riccardo Schöpf <rischoepf38@gmail.com>
2026-04-23 22:35:58 +0200

    creat new surey css done

--- src/app/component/create-survey-component/create-survey-component.ts
+++ src/app/component/create-survey-component/create-survey-component.ts
@@ -13,18 +13,25 @@ export class CreateSurveyComponent {
   private fb = inject(FormBuilder);
 
   questions = [1];
+  answer = [1, 2];
+  letters = ['A.', 'B.', 'C.', 'D.'];
 
   surveyForm = this.fb.group({
     title: ['', Validators.required],
     description: [''],
     end_at: [''],
     question: [''],
+    answer: [''],
   })
 
   addQuestion() {
     if (this.questions.length >= 6) return
     this.questions.push(1)
-    console.log('questions');
+  }
+
+  addAnswers() {
+    if (this.answer.length >= 4) return
+    this.answer.push(1)
   }
 
   cleanInput(id: string){
